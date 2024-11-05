a9ef723879090021a8d75a3dab56629a1edd01f5
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2024-11-05 15:03:45 -0300

    final

--- ex3.py
+++ ex3.py
@@ -13,5 +13,15 @@ canvas.rect(basic * 2, basic)
 canvas.new_color = "#C00000"
 canvas.paralelogram(basic * 2, basic, degrees = -60)
 canvas.new_color = "#000000"
-#canvas.paralelogram(200)
+
+div = 2
+
+canvas.go_to(line_x= -((basic / div) + ((basic / (div * 2)) / div)), line_y = -(basic / div))
+canvas.rect(basic / (div * 2), basic / div)
+canvas.go_to(line_x= (basic + div), line_y = (basic / (div * 2)))
+canvas.rect(basic / div, basic / (div * 2))
+canvas.go_to(line_x = (basic / div) + (basic / (div * 1.5)))
+canvas.rect(basic / div, basic / (div * 2))
+
+canvas.go_to(line_x = (basic / div) + (basic / (div * 1.5)) + 10)
 done()
